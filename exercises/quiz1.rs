d54055319e3ffe08a9374ea6f1589905410a592b
Zhou Ye <zhouye@stu.pku.edu.cn>
2025-01-24 09:32:58 +0800

    finish

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,20 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
+
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    if quantity > 40 {
+        quantity
+    } else {
+        quantity * 2
+    }
+}
+
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
